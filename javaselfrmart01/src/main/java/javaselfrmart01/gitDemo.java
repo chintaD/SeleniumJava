34eaf91e3660df2254155021583ed9d786f23f56
user <user@DESKTOP-G3GA6HS>
2021-01-28 21:53:50 +0530

    new class added to demo

---
+++ javaselfrmart01/src/main/java/javaselfrmart01/gitDemo.java
@@ -0,0 +1,11 @@
+package javaselfrmart01;
+
+public class gitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In Git Repo");
+
+	}
+
+}
